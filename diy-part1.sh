c160303a7546f8e545418a2d31237d22e7850d90
kmoc <32633913+kmoc@users.noreply.github.com>
2022-05-03 17:26:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -24,4 +24,3 @@ git config core.sparsecheckout true
 echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull --depth 1 origin master
 git branch --set-upstream-to=origin/master master
-git checkout dev
